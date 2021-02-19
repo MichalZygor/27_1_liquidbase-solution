c16ea4fc6eb25450e4710d6c56b2bf0845bd45ea
Michał <michal@winami.com>
2021-02-19 22:35:43 +0100

    Base environment

---
+++ src/main/java/pl/javastart/liquibasesolution/LiquibaseSolutionApplication.java
@@ -0,0 +1,13 @@
+package pl.javastart.liquibasesolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseSolutionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiquibaseSolutionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/javastart/liquibasesolution/LiquibaseSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.javastart.liquibasesolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibaseSolutionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
