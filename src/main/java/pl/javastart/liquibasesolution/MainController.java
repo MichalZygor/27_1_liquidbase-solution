f05a768aa3f6d26bbb27db2768a54135492e6a9c
Michał <michal@winami.com>
2021-02-20 01:31:57 +0100

    update - step1 finish

--- src/main/java/pl/javastart/liquibasesolution/MainController.java
+++ src/main/java/pl/javastart/liquibasesolution/MainController.java
@@ -3,9 +3,12 @@ package pl.javastart.liquibasesolution;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import pl.javastart.liquibasesolution.user.User;
 import pl.javastart.liquibasesolution.user.UserRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -19,8 +22,23 @@ public class MainController {
     @GetMapping("/")
     public String home(Model model){
         List<User> users = userRepository.findAll();
+        model.addAttribute("user", new User());
         model.addAttribute("users", users);
         return "home";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteUser(Model model, @PathVariable Long id) {
+        User user = userRepository.findById(id).orElseThrow();
+        userRepository.delete(user);
+        return "redirect:/";
+    }
+
+    @PostMapping("/add")
+    public String addTask(User user) {
+        //user.setCreatedDate(LocalDate.now());
+        userRepository.save(user);
+        return "redirect:/";
     }
 
 }
