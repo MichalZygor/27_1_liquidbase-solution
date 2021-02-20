8860156791719b3f2eb74f6dcd7a3a9f058a35d4
Michał <michal@winami.com>
2021-02-20 02:12:33 +0100

    update - step 2 - solution

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,9 @@
-INSERT INTO user(first_name, last_name)
+INSERT INTO user(first_name, last_name, email, newsletter)
 VALUES
-('Michał', 'Ciopek'),
-('Anna', 'Nowak'),
-('Paweł', 'Staszewski'),
-('Tomasz', 'Juzik'),
-('Marek', 'Fiołek'),
-('Aleksandra', 'Czupała'),
-('Katarzyna', 'Mączka');
+('Michał', 'Ciopek', 'mike@wp.pl', false ),
+('Anna', 'Nowak', 'anka@ssswp.pl', false ),
+('Paweł', 'Staszewski', 'gaska@wp.pl', false ),
+('Tomasz', 'Juzik', 'tomek@wp.pl', true ),
+('Marek', 'Fiołek', 'marek@wp.pl', false ),
+('Aleksandra', 'Czupała', 'ada@wp.pl', true ),
+('Katarzyna', 'Mączka', 'kasia@wp.pl', false );
